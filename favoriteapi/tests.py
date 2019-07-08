e6fcf4eb6238aea2d1acadb0e226e796cda396e5
Tega Okeremeta <okeremeta.tega@gmail.com>
2019-07-08 17:38:33 +0100

    chore(CI): Setup circle-ci for continuous integration
    - create the configuration file for circle-ci
    - add a mock test to try out the configuration
    
    [Delivers #167140261]

--- favoriteapi/tests.py
+++ favoriteapi/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
+from rest_framework.test import APITestCase
 # Create your tests here.
+
+
+class ciTest(APITestCase):
+    def test_sum(self):
+        assert sum([1, 2, 3]) == 6, "Should be 6"
