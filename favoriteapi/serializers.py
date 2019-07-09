f2406a7f0677d9b34c15869958ad71f666298c87
Tega Okeremeta <okeremeta.tega@gmail.com>
2019-07-09 12:29:07 +0100

    feat(auth): register a new user
    - create the endpoint for regitering a new user
    - write tests for the user registration endpoint
    
    [Finishes #167139875]

---
+++ favoriteapi/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'password'
+        )
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
--- favoriteapi/views.py
+++ favoriteapi/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework import permissions
+from .serializers import UserDataSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class RegisterUsersView(generics.CreateAPIView):
+    """
+    POST auth/register/
+    """
+    authentication_classes = ()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserDataSerializer
