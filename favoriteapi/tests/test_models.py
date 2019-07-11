8a49f413ebe4350ad84779568e829c9e030665fb
Tega Okeremeta <okeremeta.tega@gmail.com>
2019-07-11 19:22:31 +0100

    chore(model): create the favorite model
    - write the code implementation for the favorite model
    - write tests to check against the code implementation
    
    [Delivers #167228515]

--- favoriteapi/tests/test_models.py
+++ favoriteapi/tests/test_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from ..models import Category
+from django.contrib.auth import get_user_model
+from ..models import Category, Favorite
+
+User = get_user_model()
 
 
 # Test Models
@@ -11,7 +14,7 @@ class CategoryModelTest(TestCase):
     def create_category(self, category='games'):
         return Category.objects.create(category=category)
 
-    def test_object_string_representation_is_valid(self):
+    def test_object_string_representation_for_category_is_valid(self):
         category_obj = Category.objects.get(id=1)
         expected_object_name = f'{category_obj.category}'
         self.assertEqual(expected_object_name, str(category_obj))
@@ -24,3 +27,46 @@ class CategoryModelTest(TestCase):
     def test_the_category_instance(self):
         category = self.create_category()
         self.assertTrue(isinstance(category, Category))
+
+
+class FavoriteModelTest(TestCase):
+    user1 = User.objects.create(username='passo', email='pass@mail.com', password='extended')
+    category1 = Category.objects.create(category='phones')
+    data = {"size": "medium"}
+
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create(username='paulo', email='paulo@mail.com', password='extended')
+        category1 = Category.objects.create(category='phones')
+        data = {"size": "medium"}
+        Favorite.objects.create(
+            title='tecno',
+            description='This is just a china phone',
+            ranking=1,
+            metadata=data,
+            user=user1,
+            category=category1)
+
+    def create_favorite(self,
+                        title='tecno',
+                        description='This is just a china phone',
+                        ranking=1,
+                        metadata=data,
+                        user=user1,
+                        category=category1):
+        return Favorite.objects.create(
+            title=title,
+            description=description,
+            ranking=ranking,
+            metadata=metadata,
+            user=user,
+            category=category)
+
+    def test_the_favorite_instance(self):
+        favorite = self.create_favorite()
+        self.assertTrue(isinstance(favorite, Favorite))
+
+    def test_object_string_representation_for_favorite_is_valid(self):
+        favorite_obj = Favorite.objects.get(id=1)
+        expected_object_name = f'{favorite_obj.title}'
+        self.assertEqual(expected_object_name, str(favorite_obj))
