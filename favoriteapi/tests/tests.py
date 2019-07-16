fe76c27af770ea22b695a6c13ad4f690126df288
Tega Okeremeta <okeremeta.tega@gmail.com>
2019-07-16 15:10:21 +0100

    feat(favorite): update a favorite thing instance
    - create the endpoint for updating a favorite thing
    - write test to confirm the implementation works as expected
    
    [Finishes ##167258449]

--- favoriteapi/tests/tests.py
+++ favoriteapi/tests/tests.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
+from ..models import Category, Favorite
+
 
 User = get_user_model()
 
@@ -10,6 +12,8 @@ User = get_user_model()
 # Test Views
 class BaseViewTest(APITestCase):
     client = APIClient()
+
+    category1 = Category.objects.create(category='phones')
 
     def setUp(self):
         user = User.objects.create(username='paddy', email='paddy@mail.com')
@@ -19,17 +23,14 @@ class BaseViewTest(APITestCase):
         user1.set_password('fakepassword')
         user1.save()
 
-    def user_token(self):
+    def user_token(self, data):
         url = reverse(
             "auth-login",
             kwargs={
                 "version": "v1"
             }
         )
-        data = {
-            "username": "paddy",
-            "password": "fakepassword"
-        }
+        data = data
         response = self.client.post(
             url,
             data=json.dumps(data),
@@ -38,24 +39,22 @@ class BaseViewTest(APITestCase):
         token = response.data.get("token", '')
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
 
-    def user1_token(self):
-        url = reverse(
-            "auth-login",
-            kwargs={
-                "version": "v1"
-            }
-        )
-        data = {
-            "username": "pascal",
-            "password": "fakepassword"
-        }
-        response = self.client.post(
-            url,
-            data=json.dumps(data),
-            content_type="application/json"
-        )
-        token1 = response.data.get("token", '')
-        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token1)
+    def create_favorite(self,
+                        user,
+                        metadata,
+                        title='tecno',
+                        description='This is just a china phone',
+                        ranking=1,
+                        category=category1):
+        user = user
+        metadata = metadata
+        return Favorite.objects.create(
+            title=title,
+            description=description,
+            ranking=ranking,
+            metadata=metadata,
+            user=user,
+            category=category)
 
 
 class AuthUserAPITest(BaseViewTest):
@@ -144,7 +143,11 @@ class AuthUserAPITest(BaseViewTest):
 
 class CategoryAPITest(BaseViewTest):
     def test_create_category_success(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "category-list",
             kwargs={
@@ -183,7 +186,11 @@ class CategoryAPITest(BaseViewTest):
 
 class FavoriteAPITest(BaseViewTest):
     def test_create_first_favorite_thing_in_category_with_invalid_ranking_fails(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -208,7 +215,11 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_favorite_thing_success(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -236,7 +247,11 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(response.data['user']['username'], 'paddy')
 
     def test_create_favorite_thing_in_same_category_success(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -261,7 +276,11 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_favorite_thing_with_invalid_metadata_fails(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -285,30 +304,18 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_favorite_thing_with_existing_id_success(self):
-        self.user_token()
-        url = reverse(
-            "create-favorite",
-            kwargs={
-                "version": "v1"
-            }
-        )
-        data = {
-            "title": "tecno",
-            "description": "This is the first of its kind",
-            "ranking": 1,
-            "category": 1,
-        }
-        response = self.client.post(
-            url,
-            data=json.dumps(data),
-            content_type="application/json"
-        )
-        favorite_id = response.data['id']
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
+        user = User.objects.filter(username='paddy').first()
+        favorite = self.create_favorite(user=user, metadata={"size": "medium"})
         detail_url = reverse(
             "detail-favorite",
             kwargs={
                 "version": "v1",
-                "id": favorite_id
+                "id": favorite.id
             }
         )
         res = self.client.get(
@@ -319,7 +326,11 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(res.data['title'], "tecno")
 
     def test_get_favorite_thing_with_non_existing_id_fails(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         favorite_id = 1000
         detail_url = reverse(
             "detail-favorite",
@@ -336,7 +347,11 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(res.data['detail'], "Not found.")
 
     def test_delete_favorite_thing_success(self):
-        self.user_token()
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -369,7 +384,32 @@ class FavoriteAPITest(BaseViewTest):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_favorite_thing_for_a_different_user_fails(self):
-        self.user_token()
+        user = User.objects.filter(username='pascal').first()
+        favorite = self.create_favorite(user=user, metadata={"size": "medium"})
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
+        detail_url = reverse(
+            "detail-favorite",
+            kwargs={
+                "version": "v1",
+                "id": favorite.id
+            }
+        )
+        res = self.client.delete(
+            detail_url,
+            content_type="application/json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_favorite_thing_success(self):
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
         url = reverse(
             "create-favorite",
             kwargs={
@@ -377,7 +417,7 @@ class FavoriteAPITest(BaseViewTest):
             }
         )
         data = {
-            "title": "tecno",
+            "title": "xiaoming",
             "description": "This is the first of its kind",
             "ranking": 1,
             "category": 1,
@@ -389,7 +429,6 @@ class FavoriteAPITest(BaseViewTest):
         )
         favorite_id = response.data['id']
 
-        self.user1_token()
         detail_url = reverse(
             "detail-favorite",
             kwargs={
@@ -397,8 +436,191 @@ class FavoriteAPITest(BaseViewTest):
                 "id": favorite_id
             }
         )
-        res = self.client.delete(
+        data = {
+            "metadata": {
+                "color": "black"
+            }
+        }
+        res = self.client.patch(
             detail_url,
+            data=json.dumps(data),
             content_type="application/json"
         )
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("color", res.data["metadata"])
+
+    def test_update_favorite_thing_ranking_success(self):
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
+        url = reverse(
+            "create-favorite",
+            kwargs={
+                "version": "v1"
+            }
+        )
+        data = {
+            "title": "xiaoming",
+            "description": "This is the first of its kind",
+            "ranking": 1,
+            "category": 1,
+        }
+        response = self.client.post(
+            url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        favorite_id = response.data['id']
+        current_ranking = response.data['ranking']
+
+        detail_url = reverse(
+            "detail-favorite",
+            kwargs={
+                "version": "v1",
+                "id": favorite_id
+            }
+        )
+        data = {
+            "ranking": 2
+        }
+        res = self.client.patch(
+            detail_url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            res.data['message'], f'The highest ranking you can update to at the moment is {current_ranking}')
+
+    def test_update_favorite_thing_with_invalid_ranking_fails(self):
+        self.user_token(
+            data={
+                "username": "paddy",
+                "password": "fakepassword"
+            })
+        url = reverse(
+            "create-favorite",
+            kwargs={
+                "version": "v1"
+            }
+        )
+        data = {
+            "title": "xiaoming",
+            "description": "This is the first of its kind",
+            "ranking": 1,
+            "category": 1,
+        }
+        response = self.client.post(
+            url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        favorite_id = response.data['id']
+
+        detail_url = reverse(
+            "detail-favorite",
+            kwargs={
+                "version": "v1",
+                "id": favorite_id
+            }
+        )
+        data = {
+            "ranking": -2
+        }
+        res = self.client.patch(
+            detail_url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            res.data['message'], 'Sorry your ranking has to be a positive integer')
+
+    def test_update_existing_favorite_ranking_in_a_category_success(self):
+        user = User.objects.filter(username='pascal').first()
+        favorite = self.create_favorite(user=user, metadata={"size": "medium"})
+        self.user_token(
+            data={
+                "username": "pascal",
+                "password": "fakepassword"
+            })
+        url = reverse(
+            "create-favorite",
+            kwargs={
+                "version": "v1"
+            }
+        )
+        data = {
+            "title": "xiaoming",
+            "description": "This is the first of its kind",
+            "ranking": 2,
+            "category": 2,
+        }
+        self.client.post(
+            url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        detail_url = reverse(
+            "detail-favorite",
+            kwargs={
+                "version": "v1",
+                "id": favorite.id
+            }
+        )
+        data = {
+            "title": "joker",
+            "ranking": 2
+        }
+        res = self.client.patch(
+            detail_url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_update_existing_favorite_ranking_to_a_lower_value_in_a_category_success(self):
+        user = User.objects.filter(username='pascal').first()
+        favorite = self.create_favorite(user=user, metadata={"size": "medium"})
+        self.user_token(
+            data={
+                "username": "pascal",
+                "password": "fakepassword"
+            })
+        url = reverse(
+            "create-favorite",
+            kwargs={
+                "version": "v1"
+            }
+        )
+        data = {
+            "title": "xiaoming",
+            "description": "This is the first of its kind",
+            "ranking": 2,
+            "category": 2,
+        }
+        response = self.client.post(
+            url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        favorite_id = response.data['id']
+        detail_url = reverse(
+            "detail-favorite",
+            kwargs={
+                "version": "v1",
+                "id": favorite_id
+            }
+        )
+        data = {
+            "title": "joker",
+            "ranking": 1
+        }
+        res = self.client.patch(
+            detail_url,
+            data=json.dumps(data),
+            content_type="application/json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
